f4183ad10fca730ef91eed51abc3c8b95eff91b7
Marcus <marcuslee926335@gmail.com>
2019-07-28 12:00:38 -0500

    removed login, register, added new app routes

--- main.py
+++ main.py
@@ -16,80 +16,16 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body):
         self.title = title
         self.body = #body
         self.owner = owner
 
-class User(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    blog = db.relationship('Blog', backref='owner')
-
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
-@app.before_request
-def require_login():
-    allowed_routes = ['login', 'register']
-    if request.endpoint not in allowed_routes and 'email' not in session:
-        return redirect('/login')
 
 
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if user and user.password == password:
-            session['email'] = email
-            flash("Logged In")
-            return redirect('/')
-        else:
-            flash('User password incorrect, or user does not exist', 'error')
-
-    return render_template('login.html')
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        verify = request.form['verify']
-
-        #input validate user's data
-        existing_user = User.query.filter_by(email=email).first()
-        if not existing_user:
-            new_user = User(email, password)
-            db.session.add(new_user)
-            db.session.commit()
-            session['email'] = email
-            return redirect('/')
-
-        else:
-            #resp that same that user exits
-            return "<h1>Duplicate User</h1>"
-
-    return render_template('register.html')
-
-#adding page for all blog posts
-@app.route('/blog', methods=['GET'])
-def blog():
-
-    return render_template('blogpage.html')
-
-
-@app.route('/', methods =['POST', 'GET'])
-def index ():
-
-    owner = User.query.filter_by(email=session['email']).first()
+@app.route('/blog', methods =['POST', 'GET'])
+def blog ():
 
     if request.method == 'POST':
         blog_post = request.form['blog']
@@ -97,10 +33,8 @@ def index ():
         db.session.add(new_blog_post)
         db.session.commit()
 
+    return render_template('blogpage.html')
 
-    blogs = Blog.query.filter_by(completed=False,owner=owner).all()
-    #completed_tasks = Task.query.filter_by(completed=True,owner=owner).all()
-    return render_template('blogs.html',title="Build A Blog", #tasks=tasks, completed_tasks=completed_tasks)
 
 @app.route('/new-post', methods=['POST'])
 def new_post():
